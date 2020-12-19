d67824999e4933196c9b758acc28c36cadba55ad
denikond <denikond@gmail.com>
2020-12-19 17:46:27 +0300

    123

--- get_avito_page.py
+++ get_avito_page.py
@@ -2,9 +2,15 @@ import requests
 
 def main():
 
-    r = requests.get('https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty')
+    url = 'https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty'
+    fake_header =  { 'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' }
+    r = requests.get(url, headers=fake_header)
     print(r.status_code)
-    print(r)
+    print(r.content)
+    #print(r.text)
+    
+    with open('test.html', 'w', encoding='utf8') as output_file:
+        output_file.write(r.text)
 
 if __name__ == "__main__":
     main()
