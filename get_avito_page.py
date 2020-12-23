060886125f29a3d74a995aa39e98b29db54fdda8
Denis Kondrashov <denikond@gmail.com>
2020-12-24 02:32:49 +0300

    1 week ended

--- get_avito_page.py
+++ get_avito_page.py
@@ -10,13 +10,42 @@ fake_header =  { 'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple
 def get_item_data(html_text):
     soup = BeautifulSoup(html_text, 'lxml')
     item_name = soup.findAll("span", {"class": "title-info-title-text"})[0].next
-    num_sign = soup.findAll("span", {"data-marker": "item-view/item-id"})[0].next
-    date_sign = soup.findAll("div", {"class": "title-info-metadata-item-redesign"})[0].next
+    num_sign = soup.findAll("span", {"data-marker": "item-view/item-id"})
+    if len(num_sign)!=0:
+        num_sign = num_sign[0].next
+    else:
+        num_sign = ''
+    date_sign = soup.findAll("div", {"class": "title-info-metadata-item-redesign"})
+    if len(date_sign)!=0:
+        date_sign = date_sign[0].next
+    else:
+        date_sign = ''
     img_path = soup.findAll("div", {"class": "gallery-img-frame js-gallery-img-frame"})
-    addr = soup.findAll("span", {"class": "item-address__string"})[0].next
+    img_urls = [img.attrs['data-url'] for img in img_path]
+    addr = soup.findAll("span", {"class": "item-address__string"})
+    if len(addr) != 0:
+        addr = addr[0].next
+    else:
+        addr = ''
     price = soup.findAll("span", {"class": "js-item-price"})
+    if len(price)!=0:
+        price = price[0].next
+    else:
+        price = ''
     message_text = soup.findAll("div", {"class": "item-description-text"})
-
+    if len(message_text)!=0:
+        message_text = message_text[0].find_all('p')[0].next
+    else:
+        message_text = ''
+    str1 = item_name + ';' + num_sign + ';' + date_sign + ';' + img_urls[0] + ';' + addr + ';' + price + ';' + message_text
+    str2 = ''
+    for i in str1:
+        if i != '\n':
+            str2 += i
+    str2 += '\n'
+    print(str2)
+    with open('out.csv', 'a+') as of:
+        of.write(str2)
 
 
 
@@ -25,6 +54,7 @@ def get_item_page(postfix_url):
     item_url = prefix + postfix_url
     req_response = requests.get(item_url, headers=fake_header)
     if req_response.status_code == 200:
+        print(item_url)
         get_item_data(req_response.text)
     wait_time = random.uniform(4, 7)
     time.sleep(wait_time)    
@@ -34,12 +64,12 @@ def index_page_parser(html_text):
     mydivs = soup.findAll("div", {"class": "iva-item-titleStep-2bjuh"})
     n = 1 
     for div in mydivs:
-        get_item_page(div.contents[0]['href'])
         print('    ' + str(n)+ ' ' + div.text + ' || ' + div.contents[0]['href'])
+        get_item_page(div.contents[0]['href'])
         n += 1
 
 
-def get_index_page(start_section_url='https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty', pagenum_start=1, pagenum_end=6):
+def get_index_page(start_section_url='https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/videokarty', pagenum_start=1, pagenum_end=2):
     #функция
 #    fake_header =  { 'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' }
     req_response = requests.get(start_section_url, headers=fake_header)
