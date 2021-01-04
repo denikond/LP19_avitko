379258702fcbf8295725a4e48407478074da65ed
Denis Kondrashov <denikond@gmail.com>
2021-01-04 13:15:18 +0300

    rebuild_db_create
    Made creation an migrtaion DB by Miguel

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Item(db.Model):
+
+    key = db.Column(Integer, primary_key=True)
+    description = db.Column(db.String(255), index=True)
+    num_of_ad = db.Column(db.String(32), index=True, unique=True)
+    creation_date = db.Column(db.Date)
+    address = db.Column(db.String(255))
+    price = db.Column(db.Integer)
+    extended_text = db.Column(db.Text)
+
+class Image(db.Model):
+
+    key = db.Column(db.Integer, primary_key=True)
+    num_of_ad = db.Column(db.String(32), ForeignKey('Items.num_of_ad'))
+    image_path = db.Column(db.String(255))
