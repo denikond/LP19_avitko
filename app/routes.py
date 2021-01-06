c4e2a483a48cd9a4e4337f715cbe21d907797626
denikond <denikond@gmail.com>
2021-01-06 12:45:23 +0300

    Form and button

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+@app.route('/service')
+def service():
+	return render_template('service.html')
