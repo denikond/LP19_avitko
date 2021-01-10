881003cdce85ccb8d4f26fd70d82cd965a2132e3
denikond <denikond@gmail.com>
2021-01-11 01:00:52 +0300

    Extend db schema
    try add user table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Item(db.Model):
 
@@ -9,6 +11,8 @@ class Item(db.Model):
     address = db.Column(db.String(255))
     price = db.Column(db.Integer)
     extended_text = db.Column(db.Text)
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer)
 
     def __repr__(self):
         return '<num_of_ad:{} | description:{}>'.format(self.num_of_ad, self.description)
@@ -21,3 +25,21 @@ class Image(db.Model):
 
     def __repr__(self):
         return '<image_path {}>'.format(self.image_path)
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    creation_date = db.Column(db.DateTime)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    #items = db.relationship('Item', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
